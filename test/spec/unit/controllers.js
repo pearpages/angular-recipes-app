429183086a57bdc41cabe85f5abde58edf05d481
pearpages <pere@soms.cat>
2015-07-15 21:08:42 +0200

    trying unit tests

---
+++ test/spec/unit/controllers.js
@@ -0,0 +1,16 @@
+describe('Controllers: ',function(){
+        
+        var $scope, ctrl;
+        
+        beforeEach(module('myApp'));
+   
+        describe('ListCtrl',function(){
+                
+          var mockBackend, recipe;
+          
+          it('blah blah',function (){
+                  expect(1).toBe(1);
+          });
+        });
+        
+});
